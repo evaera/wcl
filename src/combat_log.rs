2e3c0b4d8b73239083e71b6ce36e50f0059c27ac
eryn L. K <eryn@eryn.io>
2024-03-29 00:43:18 -0400

    Initial commit

---
+++ src/combat_log.rs
@@ -0,0 +1,355 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub(crate) struct ReportEventPaginator {
+    pub data: Vec<CombatLogEvent>,
+    pub next_page_timestamp: Option<f64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct CombatLogEvent {
+    pub timestamp: i64,
+    pub fight: i64,
+
+    #[serde(flatten)]
+    pub ty: EventType,
+}
+
+#[allow(clippy::large_enum_variant)]
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "lowercase", tag = "type")]
+pub enum EventType {
+    #[serde(rename_all = "camelCase")]
+    ApplyBuff {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+
+        absorb: Option<i64>,
+    },
+    #[serde(rename_all = "camelCase")]
+    ApplyBuffStack {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+
+        stack: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    ApplyDebuff {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    AuraBroken {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+
+        #[serde(rename = "extraAbilityGameID")]
+        extra_ability_game_id: i64,
+
+        is_buff: bool,
+    },
+    #[serde(rename_all = "camelCase")]
+    BeginCast {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    Cast {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    CombatantInfo {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        gear: Vec<Gear>,
+
+        auras: Vec<Aura>,
+
+        expansion: String,
+
+        faction: i64,
+
+        #[serde(rename = "specID")]
+        spec_id: i64,
+
+        strength: i64,
+
+        agility: i64,
+
+        stamina: i64,
+
+        intellect: i64,
+
+        dodge: i64,
+
+        parry: i64,
+
+        block: i64,
+
+        armor: i64,
+
+        crit_melee: i64,
+
+        crit_ranged: i64,
+
+        crit_spell: i64,
+
+        haste_melee: i64,
+
+        haste_ranged: i64,
+
+        haste_spell: i64,
+
+        speed: i64,
+
+        leech: i64,
+
+        avoidance: i64,
+
+        mastery: i64,
+
+        versatility_damage_done: i64,
+
+        versatility_healing_done: i64,
+
+        versatility_damage_reduction: i64,
+
+        talent_tree: Vec<TalentTree>,
+
+        talents: Vec<Option<serde_json::Value>>,
+
+        pvp_talents: Vec<PvpTalent>,
+
+        custom_power_set: Vec<Option<serde_json::Value>>,
+
+        secondary_custom_power_set: Vec<Option<serde_json::Value>>,
+
+        tertiary_custom_power_set: Vec<Option<serde_json::Value>>,
+    },
+    #[serde(rename_all = "camelCase")]
+    Damage {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+
+        hit_type: i64,
+
+        amount: i64,
+
+        unmitigated_amount: Option<i64>,
+    },
+    #[serde(rename_all = "camelCase")]
+    EncounterStart {
+        #[serde(rename = "encounterID")]
+        encounter_id: i64,
+
+        name: String,
+
+        difficulty: i64,
+
+        size: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    Heal {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+
+        hit_type: i64,
+
+        amount: i64,
+
+        overheal: Option<i64>,
+
+        #[serde(default)]
+        tick: bool,
+    },
+    #[serde(rename_all = "camelCase")]
+    RefreshBuff {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    RefreshDebuff {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    RemoveBuff {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    ResourceChange {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+
+        resource_change: i64,
+
+        resource_change_type: i64,
+
+        other_resource_change: i64,
+
+        max_resource_amount: i64,
+
+        waste: i64,
+    },
+    #[serde(rename_all = "camelCase")]
+    Summon {
+        #[serde(rename = "sourceID")]
+        source_id: i64,
+
+        #[serde(rename = "targetID")]
+        target_id: i64,
+
+        target_instance: Option<i64>,
+
+        #[serde(rename = "abilityGameID")]
+        ability_game_id: i64,
+    },
+    #[serde(other)]
+    Unknown,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Aura {
+    pub source: i64,
+    pub ability: i64,
+    pub stacks: i64,
+    pub icon: String,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Gear {
+    pub id: i64,
+    pub quality: i64,
+    pub icon: String,
+    pub item_level: i64,
+    pub permanent_enchant: Option<i64>,
+
+    #[serde(rename = "bonusIDs")]
+    pub bonus_ids: Option<Vec<i64>>,
+
+    pub gems: Option<Vec<Gem>>,
+
+    #[serde(rename = "setID")]
+    pub set_id: Option<i64>,
+
+    pub on_use_enchant: Option<i64>,
+    pub temporary_enchant: Option<i64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Gem {
+    pub id: i64,
+    pub item_level: i64,
+    pub icon: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PvpTalent {
+    pub id: i64,
+    pub icon: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct TalentTree {
+    pub id: i64,
+    pub rank: i64,
+
+    #[serde(rename = "spellID")]
+    pub spell_id: i64,
+
+    pub icon: String,
+
+    #[serde(rename = "nodeID")]
+    pub node_id: i64,
+
+    pub spell_type: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename = "camelCase")]
+pub enum Expansion {
+    Dragonflight,
+    #[serde(other)]
+    Unknown,
+}
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use anyhow::Context;
+use graphql_client::GraphQLQuery;
+use reqwest::header;
+use serde::Deserialize;
+use std::env;
+
+pub mod combat_log;
+
+#[allow(clippy::upper_case_acronyms)]
+type JSON = serde_json::Value;
+
+const WCL_URL: &str = "https://www.warcraftlogs.com/api/v2/client";
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "generated/schema.json",
+    query_path = "src/queries/CombatLogQuery.graphql",
+    response_derives = "Debug"
+)]
+pub struct CombatLogQuery;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    dotenvy::dotenv().expect(".env file not found");
+
+    let mut headers = header::HeaderMap::new();
+    let mut auth_value =
+        header::HeaderValue::from_str(&format!("Bearer {}", &env::var("WCL_SECRET")?))?;
+    auth_value.set_sensitive(true);
+    headers.insert(header::AUTHORIZATION, auth_value);
+
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()
+        .unwrap();
+
+    let mut start_time = 364224.0;
+
+    loop {
+        println!("Start time: {start_time}");
+
+        let variables = combat_log_query::Variables {
+            report_id: "BTGmLz3pPbn4xhcJ".to_owned(),
+            fight_id: 3,
+            start_time: Some(start_time),
+        };
+        let body = <CombatLogQuery>::build_query(variables);
+        let response = client.post(WCL_URL).json(&body).send().await?;
+
+        let response: serde_json::Value = response.json().await?;
+
+        let data = combat_log::ReportEventPaginator::deserialize(
+            response
+                .get("data")
+                .and_then(|v| v.get("reportData"))
+                .and_then(|v| v.get("report"))
+                .and_then(|v| v.get("events"))
+                .context("Unknown response format")?,
+        )?;
+
+        if let Some(next_timestamp) = data.next_page_timestamp {
+            if next_timestamp <= start_time {
+                break;
+            }
+
+            start_time = next_timestamp;
+        } else {
+            break;
+        }
+    }
+
+    Ok(())
+}
